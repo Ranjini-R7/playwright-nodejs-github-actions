4ef1d3b51b0edefbd307124c79497af9fcae1151
Ranjini-R7 <ranjini.rajendran@testleaf.com>
2024-02-25 13:43:55 +0530

    Updated tests

--- tests/01-salesforce-retry-on-failure.spec.ts
+++ tests/01-salesforce-retry-on-failure.spec.ts
@@ -7,7 +7,7 @@ test.describe(`Salesforce login`,async()=>{
         await page.goto("https://login.salesforce.com/");
         await page.fill("#username","ranjini.r@testleaf.com");
         await page.fill("#password","Testleaf$1234")
-        await page.click("Login")
+        await page.click("#Login")
         
         const appLauncherLocator = page.locator(".slds-icon-waffle")
 
@@ -17,33 +17,5 @@ test.describe(`Salesforce login`,async()=>{
         await viewAllLocator.click()
     })
 
-    test(`Login to salesforce User2`,async({page})=>{
-        
-        await page.goto("https://login.salesforce.com/");
-        await page.delayedFill("#username","ranjini.r@testleaf.com");
-        await page.delayedFill("#password","Testleaf$1234")
-        await page.clickAndDelay("Login")
-        
-        const appLauncherLocator = page.locator(".slds-icon-waffle")
-
-        await appLauncherLocator.click()
-
-        const viewAllLocator = page.getByLabel("View All Applications")
-        await viewAllLocator.click()
-    })
-
-    test(`Login to salesforce User2`,async({page})=>{
-        
-        await page.goto("https://login.salesforce.com/");
-        await page.delayedFill("#username","ranjini.r@testleaf.com");
-        await page.delayedFill("#password","Testleaf$1234")
-        await page.clickAndDelay("Login")
-        
-        const appLauncherLocator = page.locator(".slds-icon-waffle")
-
-        await appLauncherLocator.click()
-
-        const viewAllLocator = page.getByLabel("View All Applications")
-        await viewAllLocator.click()
-    })
+    
 })
